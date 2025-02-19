090951d5b073faeff6a8a3cf1c82dd3f63402824
sachinpandeygurh <sachingurh@gmail.com>
2025-02-20 05:01:12 +0530

    Remove close button from Modal.Header

--- src/app/(social)/feed/(container)/home/NewsComponent.tsx
+++ src/app/(social)/feed/(container)/home/NewsComponent.tsx
@@ -113,7 +113,7 @@ const NewsComponent = () => {
       <Modal show={modalOpen} onHide={closeModal}>
         {selectedNews && (
           <>
-            <Modal.Header closeButton>
+            <Modal.Header >
               <Modal.Title>{selectedNews.title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
